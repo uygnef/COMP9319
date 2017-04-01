d1a0be30ab6d9d54484f0c3267f0966878fec53b
Yu Feng <97508691@qq.com>
2017-04-02 09:52:18 +1000

    finish get_c_from_index_file

--- ASS/ass2/main.cpp
+++ ASS/ass2/main.cpp
@@ -15,6 +15,7 @@ using namespace std;
 
 map<char, int> build_index(string input_file,string index_file);
 map<char, int> occ_counter;
+map<char, int> get_C_from_index_file(string index_file);
 
 int main(int argc, char* argv[]) {
 
@@ -27,16 +28,14 @@ int main(int argc, char* argv[]) {
         pattern[i-2] = argv[i];
     fstream  _file;
     _file.open(index_file,ios::in);
+    map<char, int> count_map;
     if(!_file)
-        map<char, int> count_map = build_index(input_file, index_file);
-    else{
-        map<char, int> count_map = read_last_line(index_file);
-    }
-
-
-
+        count_map = build_index(input_file, index_file);
+    else
+        count_map = get_C_from_index_file(index_file);
+    
     for( map<char,int>::iterator it = count_map.begin(); it != count_map.end(); ++it)
-        cout<<"key: "<<int(it->first) <<" value: "<<it->second<<endl;
+        cout<<"key: "<<it->first <<" value: "<<it->second<<endl;
 
     return 0;
 }
@@ -86,40 +85,41 @@ map<char, int> build_index(string input_file, string output_file){
 
 
 
-int read_last_line(string index_file) //copy from http://stackoverflow.com/questions/11876290/c-fastest-way-to-read-only-last-line-of-text-file
+map<char, int> get_C_from_index_file(string index_file) //copy from http://stackoverflow.com/questions/11876290/c-fastest-way-to-read-only-last-line-of-text-file
 {
 
     ifstream fin;
     fin.open(index_file);
-    if(fin.is_open()) {
-        fin.seekg(-1,ios_base::end);                // go to one spot before the EOF
+    map<char, int> C;
 
-        bool keepLooping = true;
-        while(keepLooping) {
-            char ch;
-            fin.get(ch);                            // Get current byte's data
+    
+    fin.seekg(-1,ios_base::end);                // go to one spot before the EOF
 
-            if((int)fin.tellg() <= 1) {             // If the data was at or before the 0th byte
-                fin.seekg(0);                       // The first line is the last line
-                keepLooping = false;                // So stop there
-            }
-            else if(ch == '\n') {                   // If the data was a newline
-                keepLooping = false;                // Stop at the current position.
-            }
-            else {                                  // If the data was neither a newline nor at the 0 byte
-                fin.seekg(-2,ios_base::cur);        // Move to the front of that data, then to the front of the data before it
-            }
+    bool keepLooping = true;
+    while(keepLooping) {
+        char ch;
+        fin.get(ch);                            // Get current byte's data
+
+        if((int)fin.tellg() <= 1) {             // If the data was at or before the 0th byte
+            fin.seekg(0);                       // The first line is the last line
+            keepLooping = false;                // So stop there
         }
-
-        string lastLine;
-        getline(fin,lastLine);                      // Read the current line
-
-
-        cout << "Result: " << lastLine << '\n';     // Display it
-
-        fin.close();
+        else if(ch == '\n') {                   // If the data was a newline
+            keepLooping = false;                // Stop at the current position.
+        }
+        else {                                  // If the data was neither a newline nor at the 0 byte
+            fin.seekg(-2,ios_base::cur);        // Move to the front of that data, then to the front of the data before it
+        }
     }
 
-    return 0;
+    int value;
+    int key;
+    while(!fin.eof()){
+        fin>>key>>value;
+        C.insert(make_pair(static_cast<char>(key), value));
+    }
+    
+    fin.close();
+    return C;
 }
 
