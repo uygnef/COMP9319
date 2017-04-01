fe3d88d14c34c12b46a3cd40a8d343e77a0545dc
Yu Feng <97508691@qq.com>
2017-04-01 23:45:26 +1100

    add build index

--- ASS/ass2/main.cpp
+++ ASS/ass2/main.cpp
@@ -3,7 +3,7 @@
 #include <cstring>
 #include <map>
 
-const int K_value = 1000;
+const int K_value = 10;
 const int MAX_NUM_OF_INDEX = 1000;
 
 using namespace std;
@@ -15,7 +15,6 @@ using namespace std;
 
 map<char, int> build_index(string input_file,string index_file);
 map<char, int> occ_counter;
-int size_recorder[MAX_NUM_OF_INDEX];
 
 int main(int argc, char* argv[]) {
 
@@ -26,12 +25,18 @@ int main(int argc, char* argv[]) {
     string pattern[argc-3];
     for(int i=2; i<argc; i++)
         pattern[i-2] = argv[i];
+    fstream  _file;
+    _file.open(index_file,ios::in);
+    if(!_file)
+        map<char, int> count_map = build_index(input_file, index_file);
+    else{
+        map<char, int> count_map = read_last_line(index_file);
+    }
 
-    map<char, int> count_map = build_index(input_file, index_file);
 
 
     for( map<char,int>::iterator it = count_map.begin(); it != count_map.end(); ++it)
-        cout<<"key: "<<it->first <<" value: "<<it->second<<endl;
+        cout<<"key: "<<int(it->first) <<" value: "<<it->second<<endl;
 
     return 0;
 }
@@ -52,31 +57,69 @@ map<char, int> build_index(string input_file, string output_file){
         i++;
 
         if(i>K_value){
+/*
+ * index_file's format: ASCII number of char  (space)  occ number  .....
+ *                              10                          1      .....
+ * each row calculate occ number of K*n char in bwt file. Using new line to separate.
+ */
+            for(map<char,int>::iterator it = occ_counter.begin(); it != occ_counter.end(); ++it)
+                index_file<<int(it->first)<<" "<<it->second<<" ";
 
-            int size = sizeof('a')*3;
-            int total_size = size_recorder[j-1];
-            for(map<char,int>::iterator it = occ_counter.begin(); it != occ_counter.end(); ++it){
-                index_file<<it->first<<" "<<it->second<<endl;
-                total_size += size;
-            }
-            size_recorder[j] = total_size;
-            occ_counter.clear();
+            index_file<<endl;
             j++;
+            i = 0;
         }
-
     }
 
-
-    /*
-     * index is |# a a a b b c d d d f| ------->  a:1, b:4, c:6, d:7, f:10
-     */
+    // index is |# a a a b b c d d d f| ------->  a:1, b:4, c:6, d:7, f:10
     int temp=0, pre_end=1;
     for( map<char,int>::iterator it = count_list.begin(); it != count_list.end(); ++it){
         temp = it->second;
         it->second = pre_end;
         pre_end += temp;
+        index_file<<int(it->first)<<" "<<it->second<<" ";
     }
 
     return count_list;
+}
+
+
+
+
+int read_last_line(string index_file) //copy from http://stackoverflow.com/questions/11876290/c-fastest-way-to-read-only-last-line-of-text-file
+{
+
+    ifstream fin;
+    fin.open(index_file);
+    if(fin.is_open()) {
+        fin.seekg(-1,ios_base::end);                // go to one spot before the EOF
+
+        bool keepLooping = true;
+        while(keepLooping) {
+            char ch;
+            fin.get(ch);                            // Get current byte's data
+
+            if((int)fin.tellg() <= 1) {             // If the data was at or before the 0th byte
+                fin.seekg(0);                       // The first line is the last line
+                keepLooping = false;                // So stop there
+            }
+            else if(ch == '\n') {                   // If the data was a newline
+                keepLooping = false;                // Stop at the current position.
+            }
+            else {                                  // If the data was neither a newline nor at the 0 byte
+                fin.seekg(-2,ios_base::cur);        // Move to the front of that data, then to the front of the data before it
+            }
+        }
+
+        string lastLine;
+        getline(fin,lastLine);                      // Read the current line
+
+
+        cout << "Result: " << lastLine << '\n';     // Display it
+
+        fin.close();
+    }
+
+    return 0;
 }
 
